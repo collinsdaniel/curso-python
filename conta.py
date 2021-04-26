cc8d406611ff2c33548a93d8355cfa735a5cfd97
C1277612 Collins Daniel Pereira de Meneses <c1277612@interno.bb.com.br>
2021-04-26 18:10:29 -0300

    atributos, métodos e atributos privados

--- conta.py
+++ conta.py
@@ -1,11 +1,21 @@
+from data import Data
+
 
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
-
         print("Construindo objeto ... {}".format(self))
 
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {} na data {} ".format(self.__saldo, self.__titular, Data(1, 1, 2020).formatar()))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
